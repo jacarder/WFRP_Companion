3ae7ec86f65d59f8020e240b72a218f8e4b61dc3
Jacarder <jonandrewcarder@gmail.com>
2023-01-13 17:28:10 -0500

    Created generateQuote method openai

--- src/lib/server/OpenAI.server.ts
+++ src/lib/server/OpenAI.server.ts
@@ -1,5 +1,5 @@
 import { Configuration, OpenAIApi } from 'openai';
-import type { CreateImageRequest } from 'openai';
+import type { CreateImageRequest, CreateCompletionRequest } from 'openai';
 
 const config = new Configuration({
 	apiKey: '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'
@@ -13,4 +13,18 @@ export const generateImage = async (command: string) => {
 		response_format: 'b64_json'
 	} as CreateImageRequest
 	return await openAI.createImage(request)
+}
+
+export const generateQuote = async (command: string) => {
+	const request = {
+		model: 'text-davinci-002',
+		prompt: command,
+		temperature: 0.9,
+		max_tokens: 150,
+		top_p: 1,
+		frequency_penalty: 0,
+		presence_penalty: 0.9,
+		stop: ['Human:', 'AI:']
+	} as CreateCompletionRequest;
+	return await openAI.createCompletion(request)
 }
